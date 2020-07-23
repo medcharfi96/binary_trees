50abf4dd8c7d80c4b9a2e72d1e4cca27c400a9cb
medcharfi96 <1389@holbertonschool.com>
2020-07-23 16:05:20 +0200

    add

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+/**
+*binary_tree_insert_left - insert in left
+*@parent: pointeur
+*@value: int
+*Return: binary_tree(mean true)
+*/
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+binary_tree_t *nouveau;
+if (parent == NULL)
+return (NULL);
+nouveau = malloc(sizeof(binary_tree_t));
+if (nouveau != NULL)
+{
+	if (parent->left != NULL)
+	{
+		nouveau->left = parent->left;
+		parent->left = nouveau;
+	}
+parent->left = nouveau;
+}
+return (NULL);
+}
